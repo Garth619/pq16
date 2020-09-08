a39ceefd7f479f7c55857e19b9a3c9d6dccf16fe
Garrett Cullen <Garth619@users.noreply.github.com>
2020-09-08 16:18:27 -0700

    push

--- wp-content/themes/pq16/page-templates/includes/homepage_template_parts/section-4.php
+++ wp-content/themes/pq16/page-templates/includes/homepage_template_parts/section-4.php
@@ -1,49 +1,33 @@
 <section id='section_four'>
 
-	<div id='sec_four_inner'>
+<div id='sec_four_inner'>
 
-		<span id='sec_four_title'><?php the_field( 'section_four_title' ); ?></span><!-- sec_four_title -->
+	<span id='sec_four_title'>the name<br/> New Mexico trusts</span><!-- sec_four_title -->
 
-		<div id='sec_four_slider_wrapper'>
-		
-			<div class='sec_four_arrow_left sec_four_arrow'></div><!-- sec_four_arrow -->
-
-			<div id='sec_four_slider'>
-
-			<?php if ( have_rows( 'section_four_slider' ) ) : ?>
-				<?php while ( have_rows( 'section_four_slider' ) ) : the_row(); ?>
-
-				<div class='sec_four_slide <?php the_sub_field( 'class' ); ?>'>
-
-					<a href="<?php the_field( 'section_four_case_results_page_link' ); ?>">
-
-						<?php $svg = get_sub_field( 'svg' ); ?>
-		
-						<?php if ( $svg ) { ?>
-							
-							<img src="<?php echo $svg['url']; ?>" alt="<?php echo $svg['alt']; ?>" />
-						
-						<?php } ?>
-
-						<span class='sec_four_slide_title'><?php the_sub_field( 'amount' ); ?></span><!-- sec_four_slide_title -->
-
-						<span class='sec_four_slide_type'><?php the_sub_field( 'type' ); ?></span><!-- sec_four_slide_type -->
-
-						<span class='sec_four_slide_descrip'><?php the_sub_field( 'description' ); ?></span><!-- sec_four_slide_descrip -->
-
-					</a>
-
-				</div><!-- sec_four_slide -->
-
-				<?php endwhile; ?>
-			<?php endif; ?>
-			
-			</div><!-- sec_four_slider -->
-
-			<div class='sec_four_arrow_right sec_four_arrow'></div><!-- sec_four_arrow -->
-		
-		</div><!-- sec_four_slider_wrapper -->
+	<div id='sec_four_att_wrapper'>
 	
-	</div><!-- sec_four_inner -->
+		<div id='sec_four_left'>
+		
+			<img src='<?php bloginfo('template_directory');?>/images/img-team.jpg' alt=''/>
+		
+		</div><!-- sec_four_left -->
 
+		<div id='sec_four_right'>
+		
+			<div id='sec_four_right_inner'>
+
+				<img src='<?php bloginfo('template_directory');?>/images/ico-sp-3.svg' alt=''/>
+			
+				<p>The attorneys at Ring Jimenez, P.C. will provide you with aggressive representation in court and the kindness you need during this difficult time. We understand that pursuing a legal case can be burdensome, but you are not alone.</p>
+
+				<a class="meet_att_button button" href="">Meet Attorneys</a>
+			
+			</div><!-- sec_four_right_inner -->
+		
+		</div><!-- sec_four_right -->
+	
+	</div><!-- sec_four_att_wrapper -->
+
+</div><!-- sec_four_inner -->
+	
 </section><!-- section_four -->
