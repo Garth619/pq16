d66910a1f80ec8e8601c2be1ec12637d8fe37e74
Garrett Cullen <Garth619@users.noreply.github.com>
2020-09-14 13:07:12 -0700

    push

--- wp-content/themes/pq16/page-templates/template-contact.php
+++ wp-content/themes/pq16/page-templates/template-contact.php
@@ -10,9 +10,7 @@
 
 			<h1 class='internal_title contact_title center_title'><?php the_title();?></h1><!-- page_title -->
 
-			<div id='contact_wrapper'>
-			
-			<div class='inner_subtitle'>
+			<div class='inner_subtitle under_title'>
 
 				<ul>
 					<li>Experienced</li>
@@ -21,6 +19,15 @@
 				</ul>
 	
 			</div><!-- inner_subtitle -->
+
+			<div id='contact_wrapper'>
+			
+				<span class="contact_address">901 Rio Grande Blvd NW, Suite D-224 Albuquerque, New Mexico 87104</span> 
+			
+				<a class='contact_phone' href="tel:5053733333"><span>P</span> (505) 373-3333</a><!-- contact_phone -->		
+				<span class='contact_phone'><span>F</span> (505) 340-3533</span><!-- contact_phone -->
+
+				<a class='button_two contact_directions'>Directions</a><!-- button_three -->
 			
 			</div><!-- contact_wrapper -->
 
