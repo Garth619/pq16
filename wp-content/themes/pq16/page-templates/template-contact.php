08b59a84a5cddba394710600bad74973b7278f9d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-09-14 14:49:42 -0700

    push

--- wp-content/themes/pq16/page-templates/template-contact.php
+++ wp-content/themes/pq16/page-templates/template-contact.php
@@ -22,12 +22,12 @@
 
 			<div id='contact_wrapper'>
 			
-				<span class="contact_address">901 Rio Grande Blvd NW, Suite D-224 Albuquerque, New Mexico 87104</span> 
+				<span class="contact_address">901 Rio Grande Blvd NW,<br/> Suite D-224<br/> Albuquerque, New Mexico 87104</span> 
 			
 				<a class='contact_phone' href="tel:5053733333"><span>P</span> (505) 373-3333</a><!-- contact_phone -->		
 				<span class='contact_phone'><span>F</span> (505) 340-3533</span><!-- contact_phone -->
 
-				<a class='button_two contact_directions'>Directions</a><!-- button_three -->
+				<a class='button_two contact_directions' href="" target="_blank" rel="noopener">Directions</a><!-- button_three -->
 			
 			</div><!-- contact_wrapper -->
 
