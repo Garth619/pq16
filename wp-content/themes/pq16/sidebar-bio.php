f51affecae46b2c6aa4f64d7c05f26dc04380370
Garrett Cullen <Garth619@users.noreply.github.com>
2020-09-15 14:57:19 -0700

    push

--- wp-content/themes/pq16/sidebar-bio.php
+++ wp-content/themes/pq16/sidebar-bio.php
@@ -24,35 +24,61 @@
 	</div><!-- att_bio_img -->
 
 	<?php if ( have_rows( 'attorney_accolades' ) ) : ?>
+
+		<div id='att_accolades_wrapper'>
+
+			<div id='att_view_accolades'>
 		
-		<div class="sidebar_box sidebar_bio">
+				<span>View Professional Details</span>
+		
+			</div><!-- att_view_accolades -->
+		
+			<div class="sidebar_box sidebar_bio">
 
-			<?php while ( have_rows( 'attorney_accolades' ) ) : the_row(); ?>
-				
-				<div class='sidebar_bio_list'>
-					
-					<h3><?php the_sub_field( 'title' ); ?></h3>
-					
-					<ul>
-					
-						<?php if ( have_rows( 'list_items' ) ) : ?>
-							
-							<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
-							
-							<li><?php the_sub_field( 'list_item' ); ?></li>
-							
-							<?php endwhile; ?>
-						
-						<?php endif; ?>
-				
-					</ul>
+				<?php while ( have_rows( 'attorney_accolades' ) ) : the_row(); ?>
+	
+					<div class='sidebar_bio_list'>
 			
-				</div><!-- sidebar_bio_list -->
-
-			<?php endwhile; ?>
+						<h3><?php the_sub_field( 'title' ); ?></h3>
 			
-		</div><!-- sidebar_box -->
+						<ul>
+			
+							<?php if ( have_rows( 'list_items' ) ) : ?>
+					
+								<?php while ( have_rows( 'list_items' ) ) : the_row(); ?>
+					
+									<li><?php the_sub_field( 'list_item' ); ?>
+									
+									<?php if ( have_rows( 'subitems' ) ) : ?>
 
+										<ul>
+					
+										<?php while ( have_rows( 'subitems' ) ) : the_row(); ?>
+
+											<li><?php the_sub_field( 'subitem' ); ?></li>
+
+										<?php endwhile; ?>
+
+										</ul>
+				
+									<?php endif; ?>
+									
+									</li>
+					
+								<?php endwhile; ?>
+				
+							<?php endif; ?>
+		
+						</ul>
+
+					</div><!-- sidebar_bio_list -->
+
+				<?php endwhile; ?>
+
+			</div><!-- sidebar_box -->
+		
+		</div><!-- att_accolades_wrapper -->
+		
 	<?php endif; ?>
 			
 </div><!-- sidebar_wrapper -->
